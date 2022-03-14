9414045ee58582957811251dbebbd57592fc9595
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-14 09:39:54 -0700

    mocha tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,13 @@
 const assertEqual = require(`../assertEqual`);
 const tail = require(`../tail`);
+const assert = require(`chai`).assert;
 
-const words = [1, 2, 3, 4];
-assertEqual(tail(words).length, 3);
+
+// const words = [1, 2, 3, 4];
+// assertEqual(tail(words).length, 3);
+
+describe(`#tail`, () => {
+  it(`should return [2,3,4] when argument is [1,2,3,4]`, () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3,4]);
+  });
+});
