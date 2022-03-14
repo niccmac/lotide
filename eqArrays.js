35c605b107b7962a600fc02e309e3aaabc977f4e
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-14 08:07:34 -0700

    adding eqArray test

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🪀 🪀 🪀  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🧨🧨🧨 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(firstArray, secondArray) {
   if (firstArray.length !== secondArray.length) {
     return false;
   }
-  
   for (let i = 0; i < secondArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
       return false;
@@ -20,9 +11,6 @@ const eqArrays = function(firstArray, secondArray) {
   return true;
 };
 
-(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true));
-(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false));
-(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true));
-(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false));
+module.exports = eqArrays;
 
 // first time I've had a code review with a mentor, very helpful.
