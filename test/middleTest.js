306b203c4c4c084db5d69afeaf6f44f2d3bf0bf3
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-14 09:45:51 -0700

    middle test to mocha testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,24 @@
 const assertArrayEqual = require(`../assertArraysEqual`);
 const middle = require(`../middle`);
 const assertEqual = require(`../assertEqual`);
+const assert = require(`chai`).assert;
 
-assertEqual(assertArrayEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]),"Arrays do match.");
-assertEqual(assertArrayEqual(middle([1, 2, 3, 4, 5]),[3]),"Arrays do match.");
-assertEqual(assertArrayEqual(middle([1, 2]),[]),"Arrays do match.");
+// assertEqual(assertArrayEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]),"Arrays do match.");
+// assertEqual(assertArrayEqual(middle([1, 2, 3, 4, 5]),[3]),"Arrays do match.");
+// assertEqual(assertArrayEqual(middle([1, 2]),[]),"Arrays do match.");
 
+describe(`#middle`, () => {
+  it(`[1, 2, 3, 4, 5, 6] should return Arrays do match.`, () => {
+    assert.strictEqual(assertArrayEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]),"Arrays do match.");
+  });
+});
+describe(`#middle`, () => {
+  it(`[1, 2, 3, 4, 5] should return Arrays do match.`, () => {
+    assert.strictEqual(assertArrayEqual(middle([1, 2, 3, 4, 5]),[3]),"Arrays do match.");
+  });
+});
+describe(`#middle`, () => {
+  it(`[1, 2] should return Arrays do match.`, () => {
+    assert.strictEqual(assertArrayEqual(middle([1, 2]),[]),"Arrays do match.");
+  });
+});
