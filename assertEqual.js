97f019e10b7a469fc5b4c114fda29dddaf0a0e66
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-14 07:40:14 -0700

    module.export

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return `🪀 🪀 🪀  Assertion Passed: ${actual} === ${expected}`;
+    console.log(`🪀 🪀 🪀  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return `🧨🧨🧨 Assertion Failed: ${actual} !== ${expected}`;
+    console.log(`🧨🧨🧨 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
